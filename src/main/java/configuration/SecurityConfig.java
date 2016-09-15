92ab6b0be1e76b3bc3036844e6beab569ea5f58a
ginosian <marta.ginosian@gmail.com>
2016-09-15 14:08:08 +0400

    Sets Spring boot basic configurations.

---
+++ src/main/java/configuration/SecurityConfig.java
@@ -0,0 +1,10 @@
+package configuration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Martha on 9/15/2016.
+ */
+@SpringBootApplication
+public class SecurityConfig{
+}
---
+++ src/main/java/configuration/WebConfig.java
@@ -0,0 +1,11 @@
+package configuration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Martha on 9/15/2016.
+ */
+@SpringBootApplication
+public class WebConfig extends WebMvcConfigurerAdapter{
+}
